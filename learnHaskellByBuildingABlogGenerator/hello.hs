bf11508c0495776addef5a151fea069af13edf44
abhinand <abhinandsainasari@gmail.com>
2023-02-09 19:00:45 -0600

    types

--- learnHaskellByBuildingABlogGenerator/hello.hs
+++ learnHaskellByBuildingABlogGenerator/hello.hs
@@ -1,6 +1,3 @@
-main = putStrLn myHtml
-
-myHtml = makeHtml "My Page Title" (h1_ "My Page Content" <> p_ "Lets learn Haskell")
 
 _html :: String -> String
 _html body = "<html>" <> body <> "</html>"
@@ -12,19 +9,74 @@ _head :: String -> String
 _head x = "<head>" <> x <> "</head"
 
 _title :: String -> String
-_title =  _el "title" 
-
-p_ :: String -> String
-p_ = _el "p"
-
-h1_ :: String -> String
-h1_ = _el "h1"
+_title = _el "title"
 
 _el :: String -> String -> String
-_el = \tag -> \content -> "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+_el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-makeHtml :: String -> String -> String
-makeHtml title body = _html ((_head . _title) title) <> _body body
+-- _el = \tag -> \content -> "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 wrapHtml :: String -> String
 wrapHtml content = "<html><body>" <> content <> "</body></html>"
+
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure x) (Structure y) = Structure (x <> y)
+
+render :: Html -> String
+render (Html x) = x
+
+extractStruct :: Structure -> String
+extractStruct (Structure x) = x
+
+type Title = String
+
+type Body = String
+
+type Paragrah = String
+
+type Heading1 = String
+
+html_ :: Structure -> Structure -> Html
+html_ head = Html . _el "html" . extractStruct . append_ head
+
+title_ :: Title -> Structure
+title_ = Structure . _el "title"
+
+body_ :: Body -> Structure
+body_ = Structure . _el "body"
+
+h1_ :: Heading1 -> Structure
+h1_ = Structure . _el "h1"
+
+p_ :: Paragrah -> Structure
+p_ = Structure . _el "p"
+
+head_ :: String -> Structure
+head_ = Structure . _el "head"
+
+myHtml =
+  render
+    ( html_
+        ( head_
+            ( extractStruct
+                (title_ "title")
+            )
+        )
+        ( body_
+            ( extractStruct
+                ( append_
+                    (head_ "My Heading")
+                    ( append_
+                        (p_ "Paragraph1")
+                        (p_ "Paragraph2")
+                    )
+                )
+            )
+        )
+    )
+
+main = putStrLn myHtml
