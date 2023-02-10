ff9c8f20d07b5db4b6d535a79be9385deb10c0c3
abhinand <abhinandsainasari@gmail.com>
2023-02-09 23:18:43 -0600

    ch3 complete

---
+++ learnHaskellByBuildingABlogGenerator/Html/Internal.hs
@@ -0,0 +1,79 @@
+module Html.Internal where
+
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure x) (Structure y) = Structure (x <> y)
+
+render :: Html -> String
+render (Html x) = x
+
+extractStruct :: Structure -> String
+extractStruct (Structure x) = x
+
+type Title = String
+
+type Body = String
+
+type Paragrah = String
+
+type Heading1 = String
+
+_el :: String -> String -> String
+_el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: Structure -> Structure -> Html
+html_ head = Html . _el "html" . extractStruct . append_ head
+
+title_ :: Structure -> Structure
+title_ = Structure . _el "title" . extractStruct
+
+node :: String -> Structure -> Structure
+node x = Structure . _el x . extractStruct
+
+body_ :: Structure -> Structure
+body_ = node "body"
+
+h1_ :: Structure -> Structure
+h1_ = node "h1"
+
+p_ :: Structure -> Structure
+p_ = node "p"
+
+head_ :: Structure -> Structure
+head_ = node "head"
+
+li_ :: Structure -> Structure
+li_ = node "li"
+
+listNode :: String -> [Structure] -> Structure
+listNode x = node x . foldl append_ (leaf "")
+
+ul_ :: [Structure] -> Structure
+ul_ = listNode "ul"
+
+ol_ :: [Structure] -> Structure
+ol_ = listNode "ol"
+
+code_ :: Structure -> Structure
+code_ = node "pre"
+
+leaf :: String -> Structure
+leaf = Structure . escape
+
+tleaf :: (Structure -> Structure) -> String -> Structure
+tleaf t = t . leaf
+
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concatMap escapeChar
--- learnHaskellByBuildingABlogGenerator/hello.hs
+++ learnHaskellByBuildingABlogGenerator/hello.hs
@@ -1,80 +1,16 @@
-
-_html :: String -> String
-_html body = "<html>" <> body <> "</html>"
-
-_body :: String -> String
-_body content = "<body>" <> content <> "</body>"
-
-_head :: String -> String
-_head x = "<head>" <> x <> "</head"
-
-_title :: String -> String
-_title = _el "title"
-
-_el :: String -> String -> String
-_el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
--- _el = \tag -> \content -> "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-wrapHtml :: String -> String
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
-
-newtype Html = Html String
-
-newtype Structure = Structure String
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure x) (Structure y) = Structure (x <> y)
-
-render :: Html -> String
-render (Html x) = x
-
-extractStruct :: Structure -> String
-extractStruct (Structure x) = x
-
-type Title = String
-
-type Body = String
-
-type Paragrah = String
-
-type Heading1 = String
-
-html_ :: Structure -> Structure -> Html
-html_ head = Html . _el "html" . extractStruct . append_ head
-
-title_ :: Title -> Structure
-title_ = Structure . _el "title"
-
-body_ :: Body -> Structure
-body_ = Structure . _el "body"
-
-h1_ :: Heading1 -> Structure
-h1_ = Structure . _el "h1"
-
-p_ :: Paragrah -> Structure
-p_ = Structure . _el "p"
-
-head_ :: String -> Structure
-head_ = Structure . _el "head"
+import Data.Binary.Builder (append)
+import Html
 
 myHtml =
   render
     ( html_
         ( head_
-            ( extractStruct
-                (title_ "title")
-            )
+            (tleaf title_ "<title>")
         )
         ( body_
-            ( extractStruct
-                ( append_
-                    (head_ "My Heading")
-                    ( append_
-                        (p_ "Paragraph1")
-                        (p_ "Paragraph2")
-                    )
-                )
+            ( append_
+                (tleaf p_ "<body>")
+                (tleaf p_ "Paragraph2")
             )
         )
     )
