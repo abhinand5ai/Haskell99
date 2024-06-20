7c7735899b1d2dc93539e8bf051ddf64f7b9f4e1
abhinand <abhinand.nasari@nium.com>
2024-06-19 21:12:25 -0700

    taut

--- ProgrammingInHaskell/ch08/ch08_ex.hs
+++ ProgrammingInHaskell/ch08/ch08_ex.hs
@@ -1,4 +1,9 @@
+import Control.Monad (when)
 import Data.Graph (Tree)
+import Distribution.Backpack.ModSubst (ModSubst)
+import Distribution.PackageDescription (ConfVar (Impl))
+import GHC.Exts.Heap (GenClosure (var))
+
 type Pair a = (a, a)
 
 mult :: Pair Int -> Int
@@ -32,9 +37,78 @@ safehead (x : xs) = Just x
 
 newtype Natural = N Int
 
-
 data Nat = Zero | Succ Nat
 
 data Lst a = Nil | Cons a (Lst a)
 
 data Tree' a = Leaf a | Node (Tree' a) a (Tree' a)
+
+----------------
+-- Tautolgy eval
+----------------
+
+data Prop
+  = Const Bool
+  | Var Char
+  | Not Prop
+  | And Prop Prop
+  | Imply Prop Prop
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+type Assoc k v = [(k, v)]
+
+type Subst = Assoc Char Bool
+
+rmdups :: (Eq a) => [a] -> [a]
+rmdups [] = []
+rmdups (x : xs) = x : [y | y <- xs, y /= x]
+
+find :: (Eq k) => k -> Assoc k v -> v
+find k t = head [v | (k', v) <- t, k' == k]
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b) = b
+eval s (Var x) = find x s
+eval s (Not p) = not (eval s p)
+eval s (And p q) = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+
+vars :: Prop -> [Char]
+vars (Const _) = []
+vars (Var x) = [x]
+vars (Not p) = vars p
+vars (And p q) = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n =
+  map (False :) bss ++ map (True :) bss
+  where
+    bss = bools (n - 1)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+  where
+    vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- substs p]
+-----------------------------------------------
+
+-------------------
+-- Abstract Machine
+-------------------
+
+data Expr = Val Int | Add Expr Expr
