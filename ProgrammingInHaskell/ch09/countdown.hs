286673a27dce0daebf6ac49d854bb35d2b54d722
abhinand <abhinand.nasari@nium.com>
2024-09-06 11:51:26 -0700

    ch09

--- ProgrammingInHaskell/ch09/countdown.hs
+++ ProgrammingInHaskell/ch09/countdown.hs
@@ -3,10 +3,15 @@
 -- construct and expression whose value in 765
 -- all numbers including intermediate results must be positive naturals
 -- each source number can only be used at most once
-
 -- one sol --> (25 - 10) * (50 + 1)
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
 
-data Op = Add | Sub | Mul | Div
+ops :: [Op]
+ops = [Add, Sub, Mul, Div]
 
 instance Show Op where
   show Add = "+"
@@ -21,45 +26,87 @@ apply Mul x y = x * y
 apply Div x y = x `div` y
 
 valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
+valid Add x y = x <= y
 valid Sub x y = x > y
-valid Mul _ _ = True
-valid Div x y = x `mod` y == 0
+valid Mul x y = x /= 1 && y /= 1 && x <= y
+valid Div x y = y /= 1 && x `mod` y == 0
 
-data Expr = Val Int | App Op Expr Expr
+data Expr
+  = Val Int
+  | App Op Expr Expr
 
 instance Show Expr where
   show (Val n) = show n
-  show (App o l r) =
-    brak l ++ show o ++ brak r
+  show (App o l r) = brak l ++ show o ++ brak r
     where
       brak (Val n) = show n
-      brak e = "(" ++ show e ++ ")"
-
-eval :: Expr -> [Int]
-eval (Val n) = [n | n > 0]
-eval (App o l r) =
-  [ apply o x y
-    | x <- eval l,
-      y <- eval r,
-      valid o x y
-  ]
+      brak e       = "(" ++ show e ++ ")"
 
 subs :: [a] -> [[a]]
 subs [] = []
 subs [x] = [[], [x]]
-subs (x : xs) = chs ++ [x : ch | ch <- chs]
+subs (x:xs) = chs ++ [x : ch | ch <- chs]
   where
     chs = subs xs
 
 interleave :: a -> [a] -> [[a]]
-interleave x [] = [[x]]
-interleave x (y : ys) = (x : y : ys) : map (y :) (interleave x ys)
+interleave x []     = [[x]]
+interleave x (y:ys) = (x : y : ys) : map (y :) (interleave x ys)
 
 perms :: [a] -> [[a]]
-perms [] = [[]]
-perms [x] = [[x]]
-perms (x : xs) = concatMap (interleave x) (perms xs)
+perms []     = [[]]
+perms [x]    = [[x]]
+perms (x:xs) = concatMap (interleave x) (perms xs)
 
 choices :: [a] -> [[a]]
 choices = concatMap perms . subs
+
+values :: Expr -> [Int]
+values (Val n)     = [n]
+values (App _ l r) = values l ++ values r
+
+eval :: Expr -> [Int]
+eval (Val n)     = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l, y <- eval r, valid o x y]
+
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = elem (values e) (choices ns) && eval e == [n]
+
+split :: [a] -> [([a], [a])]
+split []     = []
+split [_]    = []
+split (x:xs) = ([x], xs) : [(x : ls, rs) | (ls, rs) <- split xs]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+exprs :: [Int] -> [Expr]
+exprs [] = []
+exprs [n] = [Val n]
+exprs ns =
+  [e | (ls, rs) <- split ns, l <- exprs ls, r <- exprs rs, e <- combine l r]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+type Result = (Expr, Int)
+
+results :: [Int] -> [Result]
+results [] = []
+results [n] = [(Val n, n) | n > 0]
+results ns =
+  [ res
+  | (ls, rs) <- split ns
+  , lx <- results ls
+  , ry <- results rs
+  , res <- combine' lx ry
+  ]
+
+combine' :: Result -> Result -> [Result]
+combine' (l, x) (r, y) = [(App o l r, apply o x y) | o <- ops, valid o x y]
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n = [e | ns' <- choices ns, (e, m) <- results ns', m == n]
+
+main :: IO ()
+main = print (solutions' [1, 3, 7, 10, 25, 50] 765)
